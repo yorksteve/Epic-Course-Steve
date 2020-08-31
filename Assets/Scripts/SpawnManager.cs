a4063e1465d768d89032744d637338f381538e3e
yorkspy@gmail.com <yorkspy@gmail.com>
2020-08-31 16:31:30 -0400

    Working on SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class SpawnManager : MonoSingleton<SpawnManager>
 {
+    [SerializeField]
+    private GameObject[] _mechs;
+
+    private List<GameObject> _mechPool;
+
+    [SerializeField]
+    private GameObject _mechContainer;
+
+    private int _waveCount;
+    [SerializeField] private int _amountOfMechs;
     private Transform _startPoint;
-    [SerializeField] private GameObject[] _mechs;
 
 
     public override void Init()
@@ -13,9 +22,36 @@ public class SpawnManager : MonoSingleton<SpawnManager>
         base.Init();
     }
 
-
-    public void SpawnMechs()
+    private void Start()
     {
-
+        _mechPool = GenerateMechs(_amountOfMechs * _waveCount);
     }
+
+    List<GameObject> GenerateMechs(int amountOfMechs)
+    {
+        for (int i = 0; i < amountOfMechs; i++)
+        {
+            GameObject mech = Instantiate(_mechs[Random.Range(0, _mechs.Length)]);
+            mech.transform.parent = _mechContainer.transform;
+            mech.SetActive(false);
+            _mechPool.Add(mech);
+        }
+
+        return _mechPool;
+    }
+
+    public GameObject GetMech()
+    {
+        foreach (var mech in _mechPool)
+        {
+            if (mech.activeInHierarchy == false)
+            {
+                mech.SetActive(true);
+                return mech;
+            }
+        }
+
+        return null;
+    }
+
 }
