491feb42264079e757de5193d94b1637ca4c7753
yorkspy@gmail.com <yorkspy@gmail.com>
2020-08-31 13:41:17 -0400

    Working on SpawnManager

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoSingleton<SpawnManager>
+{
+    private Transform _startPoint;
+    [SerializeField] private GameObject[] _mechs;
+
+
+    public override void Init()
+    {
+        base.Init();
+    }
+
+
+    public void SpawnMechs()
+    {
+
+    }
+}
