64cb17ca2cc6d6a7a23090f22574103b13028d8b
yorkspy@gmail.com <yorkspy@gmail.com>
2020-09-01 14:12:31 -0400

    Finished NavMesh, working of AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,6 +8,7 @@ public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent _agent;
     private Vector3 _destination;
+    private Animator _anim;
 
     public int health;
 
@@ -15,6 +16,7 @@ public class EnemyAI : MonoBehaviour
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
 
         _destination = GameObject.Find("Destination").transform.position;
         _agent.SetDestination(_destination);
@@ -29,5 +31,21 @@ public class EnemyAI : MonoBehaviour
     void DestroyMech()
     {
         this.gameObject.SetActive(false);
+    }
+
+    void TakeDamage()
+    {
+        //health -= damageAmount;
+
+        if (health <= 0)
+        {
+            _anim.Play("Dying");
+            DestroyMech();
+        }
+    }
+
+    void Attack()
+    {
+        _anim.Play("Attacking");
     }
 }
