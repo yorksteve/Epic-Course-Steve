faf3098b7049c3329a33dff3c8d1d857d2e378d2
yorkspy@gmail.com <yorkspy@gmail.com>
2020-09-08 09:52:08 -0400

    Working on camera controls

--- Assets/Scripts/PlayerControls/CameraController.cs
+++ Assets/Scripts/PlayerControls/CameraController.cs
@@ -8,36 +8,38 @@ namespace Scripts.PlayerControls
     public class CameraController : MonoBehaviour
     {
         private float _width = Screen.width;
-        [SerializeField] private int _speed = 5;
+        private float _height = Screen.height;
+        private float _moveWidth;
+        private float _moveHeight;
+
+        [SerializeField] private int _cameraSpeed = 5;
+        [SerializeField] private int _zoomSpeed = 40;
+
+        private void Start()
+        {
+            _moveWidth = _width - 60;
+            _moveHeight = _height - 60;
+        }
 
         // Update is called once per frame
         void Update()
         {
-            KeyMovements();
-
-        }
-
-        private void KeyMovements()
-        {
-            if (Input.GetKey(KeyCode.W))
+            // Using mousewheel to zoom in and out
+            if (Input.mouseScrollDelta.y > 0)
             {
-
+                transform.Translate(Vector3.forward * Time.deltaTime * _zoomSpeed);
             }
 
-            if (Input.GetKey(KeyCode.A))
+            if (Input.mouseScrollDelta.y < 0)
             {
-                transform.Translate(Vector3.left * Time.deltaTime * _speed);
+                transform.Translate(Vector3.back * Time.deltaTime *_zoomSpeed);
             }
 
-            if (Input.GetKey(KeyCode.S))
-            {
+            // Using WSAD keys to move
+            float xTrans = Input.GetAxis("Horizontal") * Time.deltaTime * _cameraSpeed;
+            float yTrans = Input.GetAxis("Vertical") * Time.deltaTime * _cameraSpeed; //work on y, has to be angled
 
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector3.right * Time.deltaTime * _speed);
-            }
+            transform.Translate(xTrans, yTrans, 0);
         }
     }
 }
