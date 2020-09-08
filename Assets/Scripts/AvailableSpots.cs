10bedf2b477fa53a45d5f08a7b269545672d4b72
yorkspy@gmail.com <yorkspy@gmail.com>
2020-09-08 13:23:16 -0400

    Created new scripts in to begin tower placement

---
+++ Assets/Scripts/AvailableSpots.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Scripts
+{
+    public class AvailableSpots : MonoBehaviour
+    {
+        private bool _isActive;
+
+        void PlaceTower()
+        {
+            if (_isActive == false)
+            {
+                // Play particle effects
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Managers/TowerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YorkSDK.Util;
+
+namespace Scripts.Managers
+{
+    public class TowerManager : MonoSingleton<TowerManager>
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
