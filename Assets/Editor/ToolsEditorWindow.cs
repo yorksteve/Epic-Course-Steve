dfe53f640c75eeee350a29ea50878660683f805f
yorkspy@gmail.com <yorkspy@gmail.com>
2020-10-05 11:34:03 -0400

    Working on editor tool

--- Assets/Editor/ToolsEditorWindow.cs
+++ Assets/Editor/ToolsEditorWindow.cs
@@ -11,7 +11,8 @@ public class ToolsEditorWindow : EditorWindow
     private float _health;
     private float _damage;
     private float _speed;
-    private GUIStyle style;
+    private string _tag = "";
+    private float _attackRadius;
     private Object _rotationPoint;
 
     // Add menu named "My Window" to the Window menu
@@ -30,47 +31,88 @@ public class ToolsEditorWindow : EditorWindow
 
         // Ability to create and alter mechs and towers
 
-        EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutBool, "Mech", style = EditorStyles.foldoutHeader);
+        EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutBool, "Mech");
         if (_foldoutBool)
         {
+            EditorGUILayout.BeginHorizontal();
             _newColorMech = EditorGUILayout.ColorField("Mech Color", _newColorMech);
             if (GUILayout.Button("Change"))
                 ChangeColor();
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
             _newColorDissolve = EditorGUILayout.ColorField("Dissolve Color", _newColorDissolve);
-            if (GUILayout.Button("Change", style = EditorStyles.miniButton))
+            if (GUILayout.Button("Change"))
                 ChangeColor();
             // Add slider to test shader
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
             _health = EditorGUILayout.FloatField("Health", _health);
             if (GUILayout.Button("Update"))
                 ChangeHealth();
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
             _damage = EditorGUILayout.FloatField("Damage", _damage);
             if (GUILayout.Button("Update"))
                 ChangeDamage();
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
             _speed = EditorGUILayout.FloatField("Speed", _speed);
             if (GUILayout.Button("Update"))
                 ChangeSpeed();
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
             _rotationPoint = EditorGUILayout.ObjectField("Rotation Point", _rotationPoint, typeof(Transform), true);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            _tag = EditorGUILayout.TagField("Set Tag", _tag);
+            if (GUILayout.Button("Set Tag"))
+            {
+                
+            }
+            EditorGUILayout.EndHorizontal();
         }
 
         EditorGUILayout.EndFoldoutHeaderGroup();
 
         EditorGUILayout.Space();
+        EditorGUILayout.Space();
 
         EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutBool, "Tower");
         if (_foldoutBool)
         {
+            EditorGUILayout.BeginHorizontal();
             _health = EditorGUILayout.FloatField("Health", _health);
             if (GUILayout.Button("Update"))
                 ChangeHealth();
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
             _damage = EditorGUILayout.FloatField("Damage", _damage);
             if (GUILayout.Button("Update"))
                 ChangeDamage();
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            _rotationPoint = EditorGUILayout.ObjectField("Rotation Point", _rotationPoint, typeof(Transform), true);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            _tag = EditorGUILayout.TagField("Set Tag", _tag);
+            if (GUILayout.Button("Set Tag"))
+            {
+
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            _attackRadius = EditorGUILayout.FloatField("Attack Radius", _attackRadius);
+            EditorGUILayout.EndHorizontal();
         }
         EditorGUILayout.EndFoldoutHeaderGroup();
     }
